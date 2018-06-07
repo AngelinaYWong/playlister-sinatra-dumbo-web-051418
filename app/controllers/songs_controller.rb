d8f84162781ec77908c305bed0f6471bfbdccc10
github username <github email address>
2018-06-07 18:09:09 -0400

    new page, doesn't work but we can see it

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,10 +4,27 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  post "/songs/new" do
+  song = Song.new(song_params)
+  if book.save
+  # If we are successful, redirect to the show page
+  redirect "/songs/:slug"
+  else
+  # If we fail, redirect to the new form
+  redirect '/books/new'
+    end
+  end
+
   get '/songs/:slug' do
     song_slug = params["slug"]
     @song = Song.find_by_slug(song_slug)
     erb :'songs/show'
   end
+
 
 end
