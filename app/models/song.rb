ed837bb6196c2f9ec321fedc676a8841422c7311
github username <github email address>
2018-06-07 15:39:15 -0400

    slugs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
-  has_many :genres, through: :song_genre
+  has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|name| name.slug == slug}
+  end
+
 end
